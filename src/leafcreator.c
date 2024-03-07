45c1c25e24a94697b06664f53c001128e23dafe2
Hawthorn Emerald <hawthornemerald100@gmail.com>
2024-03-07 20:55:45 +0800

    removed -c arg and can now read data from stdin

--- src/leafcreator.c
+++ src/leafcreator.c
@@ -12,11 +12,11 @@ int cmpfunc (const void * a, const void * b) {
 	
 int print_help() {
   printf("stem-leaf plot generator: 23:25 -> 2 | 3 5 ");
-  printf("\nusage: slpg [data] -c [count] [-nr]");
+  printf("\nusage: slpg [data] [-nr]");
   printf("\ninclude parameter -r for stem on RIGHT and leaves on LEFT");
   printf("\ninclude parameter -n to omit/hide stem in output");
   printf("\ninclude parameter -h to show help message");
-  printf("\n**needs** parameter -c [data element count]");
+//printf("\n**needs** parameter -c [data element count]");
   printf("\nsplit elements in data with ':'\n");
   return 0;
 }
@@ -26,15 +26,20 @@ int main(int argc, char *argv[]) {
   int right_to_left = 0;
   int no_stem = 0;
   int stem_num_begin = 0;
-  int datastr_index = -1;
+  // -1: stdin
+  // -2: none
+  int datastr_index = -2;
   int ptcount = 0;
+  int i;
+  int j;
 
-  for (int i = 0; i < argc; i++) {
+  for (i = 1; i < argc; i++) {
     if (strcmp(argv[i], "-h") == 0) {
       print_help();
       return 0;
     }
     if (argv[i][0] != '-') {
+      // if data is argument
       datastr_index = i;
     }
     if (strcmp(argv[i], "-r") == 0) {
@@ -43,31 +48,61 @@ int main(int argc, char *argv[]) {
     if (strcmp(argv[i], "-n") == 0) {
       no_stem = 1;
     }
-    if (strcmp(argv[i], "-c") == 0
-      && i+1 < argc) {
-      ptcount = atoi(argv[++i]);
+//  if (strcmp(argv[i], "-c") == 0
+//    && i+1 < argc) {
+//    ptcount = atoi(argv[++i]);
+//  }
+  }
+
+  // if data is not in argument, get from stdin 
+  char* stdin_buffer = NULL;
+  if (datastr_index == -2) {
+    // printf("No data string in arguments found\n");
+    datastr_index = -1;
+    stdin_buffer = malloc(30);
+    i = 0;
+    while (!feof(stdin)) {
+      if (++i%30 == 29) {
+        char* temp_sb_pt = realloc(stdin_buffer, i+30);
+        if (temp_sb_pt == NULL) {
+          fprintf(stderr, "Could not allocate memory to continue reading");
+          break;
+        }
+        stdin_buffer = temp_sb_pt;
+      }
+      fread(stdin_buffer, sizeof(stdin_buffer), 1, stdin);
+      // printf("%d", i);
     }
   }
-  
-  if (datastr_index < 0 || argc <= 1) {
+
+  char* datastr = (datastr_index == -1) ? stdin_buffer : argv[datastr_index];
+
+  if (datastr_index == -2 /* || argc <= 1 */) {
     print_help();
     return 0;
   }
+
+  // find number of colons in string: # + 1 = ptcount
+  for (i = 0; i < strlen(datastr); i++) {
+    if (datastr[i] == ':') ptcount++;
+  }
+  ptcount++;
 
   int* data = (int *) malloc(ptcount * sizeof (int));
   int* datastem = (int *) malloc(ptcount * sizeof (int)); 
 
   char* token;
   int fart = 0;
-  int i;
-  int j;
-  token = strtok(argv[datastr_index], ":");
-  for (i = 0; i < ptcount; i++) {
+  token = strtok(
+      datastr,
+      ":");
+  for (i = 0; token != NULL; i++){
     fart = atoi(token);
     data[i] = fart;
     datastem[i] = fart / 10;
     token = strtok(NULL, ":");
   }
+  ptcount = i;
 
   // printf("Sorting... ");
   qsort(data, ptcount, sizeof(int), cmpfunc);
@@ -118,5 +153,6 @@ int main(int argc, char *argv[]) {
 
   free(data);
   free(datastem);
+  free(stdin_buffer);
   return 0;
 }
