1323b580cce8d44e438c7351d5fd71b8231a0bc1
Hawthorn Emerald <hawthornemerald100@gmail.com>
2024-03-20 22:16:04 +0800

    add decimal support and more flags

--- src/leafcreator.c
+++ src/leafcreator.c
@@ -6,18 +6,24 @@
 // Copyleft NORTRITECH CORPORATION
 // TODO: add support for stdin
 // if equal returns 0
-int cmpfunc (const void * a, const void * b) {
-   return ( *(int*)a - *(int*)b );
+int cmpfunc (const void * _a, const void * _b) {
+   float a = *(float*)_a;
+   float b = *(float*)_b;
+   float diff = a-b;
+   return fabs(diff) < 0.00005 ?
+     0 : (int)roundf(diff/fabs(diff)); // pos/pos = 1, pos/neg = -1
 }
 	
 int print_help() {
   printf("stem-leaf plot generator: 23:25 -> 2 | 3 5 ");
-  printf("\nusage: slpg [data] [-nr]");
-  printf("\ninclude parameter -r for stem on RIGHT and leaves on LEFT");
-  printf("\ninclude parameter -n to omit/hide stem in output");
-  printf("\ninclude parameter -h to show help message");
-//printf("\n**needs** parameter -c [data element count]");
-  printf("\nsplit elements in data with ':'\n");
+  printf("\nusage: slpg [data] [-nr] [-f factor]");
+  printf("\nsplit elements in data with ':'");
+  printf("\n -r for stem on RIGHT and leaves on LEFT");
+  printf("\n -n to omit/hide stem in output");
+  printf("\n -h to show help message");
+  printf("\n -d to print leaf values as decimal (e.g. 2 | 1.2)");
+  printf("\n -f [num]: change factor of stem (e.g. 10: tens place in stem)");
+  printf("\n -s [num]: change starting stem\n");
   return 0;
 }
 
@@ -25,14 +31,16 @@ int main(int argc, char *argv[]) {
   // output style variables
   int right_to_left = 0;
   int no_stem = 0;
+  int print_dec = 0;
   int stem_num_begin = 0;
   // -1: stdin
   // -2: none
   int datastr_index = -2;
+  int factor = 10;
   int ptcount = 0;
   int i;
   int j;
-
+  
   for (i = 1; i < argc; i++) {
     if (strcmp(argv[i], "-h") == 0
         || strcmp(argv[i], "--help") == 0) {
@@ -49,10 +57,17 @@ int main(int argc, char *argv[]) {
     if (strcmp(argv[i], "-n") == 0) {
       no_stem = 1;
     }
-//  if (strcmp(argv[i], "-c") == 0
-//    && i+1 < argc) {
-//    ptcount = atoi(argv[++i]);
-//  }
+    if (strcmp(argv[i], "-d") == 0) {
+      print_dec = 1;
+    }
+    if (strcmp(argv[i], "-f") == 0 
+      && i+1 < argc) {
+      factor = atoi(argv[++i]);
+    }
+    if (strcmp(argv[i], "-s") == 0 
+      && i+1 < argc) {
+      stem_num_begin = atoi(argv[++i]);
+    }
   }
 
   // if data is not in argument, get from stdin 
@@ -94,46 +109,47 @@ int main(int argc, char *argv[]) {
   }
   ptcount++;
 
-  int* data = malloc(ptcount * sizeof (int));
-  int* datastem = malloc(ptcount * sizeof (int)); 
+  float* data = (float *) malloc(ptcount * sizeof (float));
+  int* datastem = (int* )malloc(ptcount * sizeof (int)); 
 
   char* token;
-  int fart = 0;
-  token = strtok(
-      datastr,
-      ":");
+  float fart = 0;
+  token = strtok(datastr, ":");
   for (i = 0; token != NULL; i++){
-    fart = atoi(token);
+    fart = atof(token);
     data[i] = fart;
-    datastem[i] = fart / 10;
+    datastem[i] = (int)(fart / factor);
     token = strtok(NULL, ":");
   }
   ptcount = i;
 
   // printf("Sorting... ");
-  qsort(data, ptcount, sizeof(int), cmpfunc);
+  qsort(data, ptcount, sizeof(float), cmpfunc);
   qsort(datastem, ptcount, sizeof(int), cmpfunc); 
-  /*
-  for (i = 0; i < ptcount; i++) {
-    printf("%d ", data[i]);
-  }
-  printf("\n\n");
-  */
   
+  // print warning if starting stem cuts off data
+  if (data[0] < stem_num_begin * factor) {
+    fprintf(stderr,
+        "WARNING: smallest data point %.1f is lower than minimum %d\n",
+        data[0], stem_num_begin*factor);
+  }
+
   // stem-leaf matrix: [stem][leafindex]
-  // eg:
-  // 0 | 1 2 3
-  // 1 | 4 4 5
-  // i iterates 0 -> maxstem
-  int sl_matrix[datastem[ptcount-1]+1][ptcount];
-  for (i = 0; i <= datastem[ptcount-1]; i++) {
+  // e.g. with stem_num_begin as 2
+  // 0(2) | 1.0 2.1 3.0
+  // 1(3) | 4.0 4.1 5.2
+  // stem_num_begin -> maxstem (below applied stem_num_begin offset to start at 0)
+  // i iterates 0 -> maxstem - stem_num_begin
+  float sl_matrix[datastem[ptcount-1]+1][ptcount];
+  for (i = 0; i <= datastem[ptcount-1] - stem_num_begin; i++) {
     for (j=0;j < ptcount;j++){
-      sl_matrix[i][j]=-69;
+      sl_matrix[i][j]= -1.0;
     }
   }
   int current_leaf_no = 0;
   for (i = 0; i < ptcount; i++) {
-    sl_matrix[datastem[i]][current_leaf_no] = data[i] - data[i]/10*10;
+    sl_matrix[datastem[i] - stem_num_begin][current_leaf_no]
+      = fmodf(data[i], (float)factor);
     if (i < ptcount-1) {
       if (datastem[i+1]==datastem[i]) current_leaf_no++;
       else current_leaf_no = 0;
@@ -141,17 +157,17 @@ int main(int argc, char *argv[]) {
   }
   
   // ternary bool ? r-l : l-r
-  for (i = 0; i <= datastem[ptcount-1]; i++) {
-    if (!no_stem && !right_to_left) printf("%d | ", i);
+  for (i = 0; i <= datastem[ptcount-1] - stem_num_begin; i++) {
+    if (!no_stem && !right_to_left) printf("%d | ", i + stem_num_begin);
 //         (right_to_left ? ptcount-1 : 0)  
     for (j = (ptcount-1)*right_to_left; 
       right_to_left ? j >= 0 : j < ptcount;
 //         (right_to_left ? -1 : 1)   
       j += 1-2*right_to_left) {
-      if (sl_matrix[i][j] < 10 && sl_matrix[i][j] >= 0)
-        printf("%d ", sl_matrix[i][j]);
+      if (sl_matrix[i][j] < factor && sl_matrix[i][j] >= 0)
+        printf(print_dec ? "%3.1f " : "%1.0f ", sl_matrix[i][j]);
     }
-    if (!no_stem && right_to_left) printf("| %d", i);
+    if (!no_stem && right_to_left) printf("| %d", i + stem_num_begin);
     printf("\n");
   }
   
